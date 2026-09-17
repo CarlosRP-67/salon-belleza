565c31c928e2e46692d338f4acc2925a61e58e22
CarlosRP-67 <carlosrodyduke@gmail.com>
2026-09-17 15:51:44 -0600

    Implementación de usuarios y vista de registro

--- src/main/java/com/beauty/spa/salon/App.java
+++ src/main/java/com/beauty/spa/salon/App.java
@@ -1,20 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package main.java.com.beauty.spa.salon;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Carlitos
  */
-public class App {
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Carga la vista FXML desde la carpeta resources/view
+            Parent root = FXMLLoader.load(getClass().getResource("/main/resources/view/registro-view.fxml"));
+            
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Salón de Belleza - Registro de Usuarios");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+        } catch (Exception e) {
+            System.err.println("Error al iniciar la aplicación: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
-    
 }
